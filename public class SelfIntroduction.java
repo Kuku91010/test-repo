d4ac77eed57afd69d90fb970dd1ea87ec34a1716
Kuku91010 <marumaru62@hufs.ac.kr>
2026-03-11 17:14:45 +0900

    Add files via upload

---
+++ public class SelfIntroduction.java
@@ -0,0 +1,11 @@
+public class SelfIntroduction {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        int age = 25;
+        String hobby = "프로그래밍";
+
+        System.out.println("안녕하세요, 제 이름은 " + name + "입니다.");
+        System.out.println("나이는 " + age + "살이고,");
+        System.out.println("취미는 " + hobby + "입니다.");
+    }
+}
