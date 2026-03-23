66ab0041b0d13ef416e33fc9290bac64ba581680
junehuk <93902605+junehuk@users.noreply.github.com>
2026-03-24 01:09:44 +0900

    Update public class SelfIntroduction.java

--- public class SelfIntroduction.java
+++ public class SelfIntroduction.java
@@ -7,7 +7,7 @@ public class SelfIntroduction {
 
         System.out.println("안녕하세요, 제 이름은 " + name + "입니다.");
         System.out.println("나이는 " + age + "살이고,");
-        System.out.println("취미는 " + hobby + "입니다.");
+        //System.out.println("취미는 " + hobby + "입니다.");
     }
 
 }
